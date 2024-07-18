352c1d16f909145c07e9bcdb13b745082d8d7149
HoangTe <doanhuyhoang11a2ntt2020@gmail.com>
2024-07-18 08:23:58 +0700

    feat: code

---
+++ index.js
@@ -0,0 +1,17 @@
+var  p = 10;
+var g = 0;
+var l = setInterval(delayAlert, 1000);
+function delayAlert() {
+    if (g < 10)
+        console.log(p + " : 0" + g);
+    else
+        console.log(p + " : " + g);
+    g--;
+    if (g < 0) {
+        g = 59;
+        p--;
+    }
+    if (p < 0) {
+        clearInterval(l);
+    }
+}
