1f8b428524eabc121f04cac0b97eb9725d7f177f
HoangTe <doanhuyhoang11a2ntt2020@gmail.com>
2024-07-25 20:44:46 +0700

    feat:update

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
-// var level = 0;
-var f = document.getElementById("fan");
-var rotation = 0;
-var speed = 0;
+// // var level = 0;
+// var f = document.getElementById("fan");
+// var rotation = 0;
+// var speed = 0;
 
-setInterval(function (){
-    rotation += speed;
-    f.style.transform = "rotate(" + -rotation + "deg)";
-}, 10);
+// setInterval(function (){
+//     rotation += speed;
+//     f.style.transform = "rotate(" + -rotation + "deg)";
+// }, 10);
 
-function offFan() {
-    speed = 0;
+// function offFan() {
+//     speed = 0;
+// }
+// function onFan(s) {
+// speed = {
+//     1: 10,
+//     2: 20,
+//     3: 30,
+// }[s];
+// }
+
+// function openSubmenu(e) {
+//     hideSubmenu();
+//     var sub = e.lastElementChild;
+//     sub.style.display = "block";
+// }
+// function hideSubmenu() {
+//     var subs = document.getElementsByClassName('submenu');
+//     for (var s of subs) {
+//         s.style.display = "none";
+//     }
+// }
+
+function showLoginForm(event) {
+    event.preventDefault();
+    document.querySelector('.login-container').style.display = 'block';
 }
-function onFan(s) {
-speed = {
-    1: 10,
-    2: 20,
-    3: 30,
-}[s];
+function hideLoginForm() {
+    document.querySelector('.login-container').style.display = 'none';
 }
