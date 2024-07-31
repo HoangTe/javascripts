c7d3c353dea5c6438a3801c8e8aeb1be213330f0
HoangTe <doanhuyhoang11a2ntt2020@gmail.com>
2024-08-01 00:04:34 +0700

    feat: sort and limit

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-// var level = 0;
-// var f = document.getElementById("fan");
-// var rotation = 0;
-// var speed = 0;
-
-// setInterval(function (){
-//     rotation += speed;
-//     f.style.transform = "rotate(" + -rotation + "deg)";
-// }, 10);
-
-// function offFan() {
-//     speed = 0;
-// }
-// function onFan(s) {
-// speed = {
-//     1: 10,
-//     2: 20,
-//     3: 30,
-// }[s];
-// }
 
 // function openSubmenu(e) {
 //     hideSubmenu();
