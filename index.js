d12ac6ef608103d0d6bb448dadd277dd602354a1
HoangTe <doanhuyhoang11a2ntt2020@gmail.com>
2024-07-31 23:57:26 +0700

    sort and limif

--- index.js
+++ index.js
@@ -1,86 +1,160 @@
 // var level = 0;
-var f = document.getElementById("fan");
-var rotation = 0;
-var speed = 0;
+// var f = document.getElementById("fan");
+// var rotation = 0;
+// var speed = 0;
 
-setInterval(function (){
-    rotation += speed;
-    f.style.transform = "rotate(" + -rotation + "deg)";
-}, 10);
+// setInterval(function (){
+//     rotation += speed;
+//     f.style.transform = "rotate(" + -rotation + "deg)";
+// }, 10);
 
-function offFan() {
-    speed = 0;
-}
-function onFan(s) {
-speed = {
-    1: 10,
-    2: 20,
-    3: 30,
-}[s];
-}
-
-function openSubmenu(e) {
-    hideSubmenu();
-    var sub = e.lastElementChild;
-    sub.style.display = "block";
-}
-function hideSubmenu() {
-    var subs = document.getElementsByClassName('submenu');
-    for (var s of subs) {
-        s.style.display = "none";
-    }
-}
-
-function showLoginForm(event) {
-    event.preventDefault();
-    document.querySelector('.login-container').style.display = 'block';
-}
-function hideLoginForm() {
-    document.querySelector('.login-container').style.display = 'none';
-}
-
-// function enterKeyDown(){
-//     var ipx = document.getElementById("ipx");
-//     var v = ipx.value;
-//     console.log(`Key down...:${v}`);
+// function offFan() {
+//     speed = 0;
 // }
-// function enterKeyUp(e){
-//     var v = e.value;
-//     console.log(`Key up...:${v}`);
-//     if(Event.keyCode == 13 && v.length < 6){
-//         var msg = document.getElementById('msg');
-//         msg.innerText = "Nhập tối thiểu 6 kí tự";
-//         msg.style.display='block';
+// function onFan(s) {
+// speed = {
+//     1: 10,
+//     2: 20,
+//     3: 30,
+// }[s];
+// }
+
+// function openSubmenu(e) {
+//     hideSubmenu();
+//     var sub = e.lastElementChild;
+//     sub.style.display = "block";
+// }
+// function hideSubmenu() {
+//     var subs = document.getElementsByClassName('submenu');
+//     for (var s of subs) {
+//         s.style.display = "none";
 //     }
 // }
 
-const emailInput = document.getElementById('email');
-const emailError = document.getElementById('email-error');
-const passwordInput = document.getElementById('password');
-const passwordError = document.getElementById('password-error');
-emailInput.addEventListener('input', validateEmail);
-passwordInput.addEventListener('input', validatePassword);
+// function showLoginForm(event) {
+//     event.preventDefault();
+//     document.querySelector('.login-container').style.display = 'block';
+// }
+// function hideLoginForm() {
+//     document.querySelector('.login-container').style.display = 'none';
+// }
 
-function validateEmail(){
-    const email = emailInput.value;
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    if (!emailRegex.test(email)) {
-        emailError.textContent = "Invalid email format";
-        emailError.style.color = "red";
-    } else {
-        emailError.textContent = "Exaclly";
-        emailError.style.color = "green";
+// // function enterKeyDown(){
+// //     var ipx = document.getElementById("ipx");
+// //     var v = ipx.value;
+// //     console.log(`Key down...:${v}`);
+// // }
+// // function enterKeyUp(e){
+// //     var v = e.value;
+// //     console.log(`Key up...:${v}`);
+// //     if(Event.keyCode == 13 && v.length < 6){
+// //         var msg = document.getElementById('msg');
+// //         msg.innerText = "Nhập tối thiểu 6 kí tự";
+// //         msg.style.display='block';
+// //     }
+// // }
+
+// const emailInput = document.getElementById('email');
+// const emailError = document.getElementById('email-error');
+// const passwordInput = document.getElementById('password');
+// const passwordError = document.getElementById('password-error');
+// emailInput.addEventListener('input', validateEmail);
+// passwordInput.addEventListener('input', validatePassword);
+
+// function validateEmail(){
+//     const email = emailInput.value;
+//     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+//     if (!emailRegex.test(email)) {
+//         emailError.textContent = "Invalid email format";
+//         emailError.style.color = "red";
+//     } else {
+//         emailError.textContent = "Exaclly";
+//         emailError.style.color = "green";
+//     }
+// }
+
+// function validatePassword(){
+//     const password = passwordInput.value;
+//     if(password.length < 6){
+//         passwordError.textContent = "Password must be at least 6 characters";
+//         passwordError.style.color = 'red';
+//     }else{
+//         passwordError.textContent = "Exaclly";
+//         passwordError.style.color = 'green';
+//     }
+
+// }
+
+
+
+// var url = `https://dummyjson.com/products/category-list`;
+// fetch(url).then(function(rs){
+//     return rs.json();
+// })
+// .then(function(rs){
+//     console.log(rs);
+//     var menu = document.getElementById("menu");
+//     for(var item of rs){
+//         var item_html = `<li class="item">
+//                             <a href="#">${item}</a>
+//                         </li>`;
+//         menu.innerHTML += item_html;
+//     }
+// })
+
+var url = `https://dummyjson.com/products`;
+fetch(url).then(function(rs){
+    return rs.json();
+})
+.then(function(rs){
+    var products = rs.products;
+    var list = document.getElementById("list-products");
+    for(var p of products){
+        var p_html = `<div class="col-3 mb-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="${p.thumbnail}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">${p.title}</h5>
+                          <h6 class="card-title">${p.price}</h6>
+                          <p class="card-text">${p.description}</p>
+                          <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                      </div>
+                </div>`;
+        list.innerHTML += p_html;
+    }    
+})
+
+function searchProduct(e) {
+    if(event.key === "Enter" || e.id === "product-limit" || e.id === "product-sort"){
+        var keyword = e.value;
+        var limit = document.getElementById("product-limit").value; 
+        var sort = document.getElementById("product-sort").value; 
+        var url_search = `https://dummyjson.com/products/search?q=${keyword}&limit=${limit}&sortBy=price&order=${sort}`;
+        fetch(url_search)
+        .then(function(rs) {
+            return rs.json();
+        })
+        .then(function(rs) {
+            var products = rs.products;
+            var list = document.getElementById("list-products");
+            list.innerHTML = "";
+
+            for (var p of products) {
+                var p_html = `<div class="col-3 mb-3">
+                                <div class="card" style="width: 18rem;">
+                                    <img src="${p.thumbnail}" class="card-img-top" alt="...">
+                                        <div class="card-body">
+                                            <h5 class="card-title">${p.title}</h5>
+                                            <h6 class="card-title">${p.price}</h6>
+                                            <p class="card-text">${p.description}</p>
+                                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                    </div>
+                                </div>`;
+                list.innerHTML += p_html;
+            }
+        });
     }
 }
 
-function validatePassword(){
-    const password = passwordInput.value;
-    if(password.length < 6){
-        passwordError.textContent = "Password must be at least 6 characters";
-        passwordError.style.color = 'red';
-    }else{
-        passwordError.textContent = "Exaclly";
-        passwordError.style.color = 'green';
-    }
-
-}
